08a382001cc8d79ee796dd9b5d88396ca07fdab6
killer-cf <costa.kilder@live.com>
2023-07-01 14:56:17 -0300

    alias paths

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,5 +1,4 @@
-import { expect, test } from "vitest";
-import { Slug } from "./slug";
+import { Slug } from "./slug"
 
 test('should be able to create a new slug from test data', () => {
   const slug = Slug.createFromText('Example question title')
