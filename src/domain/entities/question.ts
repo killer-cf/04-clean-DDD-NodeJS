ad8b4500c41e019cac90ae163fd279940542f80a
killer-cf <costa.kilder@live.com>
2023-07-01 11:17:09 -0300

    mapping proprieties

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  authorId: string
+  authorId: UniqueEntityId
+  bestAnswerId: UniqueEntityId
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
