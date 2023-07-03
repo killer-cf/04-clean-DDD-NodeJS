1748d4a1ef8380c3dd90340d4fe0304e9bd68dba
killer-cf <costa.kilder@live.com>
2023-07-03 11:02:45 -0300

    structure refactor

--- src/domain/forum/aplication/use-cases/answer-question.ts
+++ src/domain/forum/aplication/use-cases/answer-question.ts
@@ -1,5 +1,5 @@
-import { UniqueEntityId } from '../../core/entities/unique-entity-id'
-import { Answer } from '../entities/answer'
+import { UniqueEntityId } from '@/core/types/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../reposiories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
