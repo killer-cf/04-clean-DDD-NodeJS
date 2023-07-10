88feb32f83f7fb078e21a6895e9feb9b07222ce2
killer-cf <costa.kilder@live.com>
2023-07-10 15:31:03 -0300

    use case comment on question

---
+++ src/domain/forum/aplication/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  test('should be able to comment on question', async () => {
+    const question = makeQuestion()
+
+    inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      authorId: question.authorId.toString(),
+      questionId: question.id.toString(),
+      content: 'comment test',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'comment test',
+    )
+  })
+})
