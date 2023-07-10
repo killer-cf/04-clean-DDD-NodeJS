000ae2bfd370f428c294d17186e3bf2d1bee33b4
killer-cf <costa.kilder@live.com>
2023-07-10 15:36:52 -0300

    use case comment on answer

---
+++ src/domain/forum/aplication/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,35 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  test('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      authorId: answer.authorId.toString(),
+      answerId: answer.id.toString(),
+      content: 'comment test',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'comment test',
+    )
+  })
+})
