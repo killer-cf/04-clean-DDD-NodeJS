39c12aea9e67074aa09e3dd874b1771d7d4e4765
killer-cf <costa.kilder@live.com>
2023-07-17 15:57:33 -0300

    domain events

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,12 @@ export abstract class Entity<Props> {
     this._id = id ?? new UniqueEntityId(id)
     this.props = props
   }
+
+  public equals(entity: Entity<any>) {
+    if (entity === this) return true
+
+    if (entity.id === this._id) return true
+
+    return false
+  }
 }
